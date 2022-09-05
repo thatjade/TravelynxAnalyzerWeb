884e01724201953350a7f59e3ae767bff6025320
Jade Schwan <jadeschwan@transrights.fritz.box>
2022-09-05 20:01:18 +0200

    added comments and THA as Fernverkehr

--- main.js
+++ main.js
@@ -18,21 +18,29 @@ window.onload = function(){
         //Parse JSON into result variable
         var result = JSON.parse(e.target.result);
 
+        //List of Train Types (e.g. ICE)
         var typelist = [];
+        //List of Places (e.g. Frankfurt (Main) Flughafen Fernbahnhof)
         var citylist = [];
 
+        //Fill the List of Train Types
         for(var i in result){
           typelist.push(result[i].type);
         }
+
+        //Fill the List of Places
         for(var i in result){
           citylist.push(result[i].to_name);
         }
 
+        //Variable that counts all rides
         var allrides = typelist.length;
 
+        //Make a List with removed Duplicates (e.g. ["ICE"],["ICE"],["RE"],["ICE"],["RE"] > ["ICE"],["RE"])
         typelistsorted =new Set(typelist)
         citylistsorted =new Set(citylist)
 
+        //Create a List of Traintypes with a counter of its occurances
         var typelistwithcounter = [];
 
         for (type of typelistsorted){
@@ -48,6 +56,7 @@ window.onload = function(){
 
         }
 
+        //Create a Places of Traintypes with a counter of its occurances
         var citylistwithcounter = [];
 
         for (city of citylistsorted){
@@ -62,26 +71,27 @@ window.onload = function(){
           citylistwithcounter.push([citycounter,city]);
         }
         
-        
+        //Create Sorted List that sorts the list descending by the counter
         sortedtypelistwithcounter = typelistwithcounter.sort(function(a,b) { return b[0] - a[0]; });
         sortedcitylistwithcounter = citylistwithcounter.sort(function(a,b) { return b[0] - a[0]; });
         
+
+        //function to round number to 2 decimals 
         function roundToTwo(num) {
           return +(Math.round(num + "e+2")  + "e-2");
         }
 
+        //Create List for Verkehrsklassifizierung
         var Klassifizierung = [];
 
-        
-
-        var FVFilter = ["ICE","IC","FLX","EC","ECE"];
+        //Filters for Classification
+        var FVFilter = ["ICE","IC","FLX","EC","ECE","THA"];
         var NVFilter = ["RB","HLB","S","RE","VIA","RT","TL","FEX","ME","WFB","TLX"];
         var BusFilter = ["Bus"];
 
+        //Adding the Fernverkehr Counter to the List
         var Fernverkehr = 0;
         for (type of FVFilter){
-
-          
           for (currentitem of result){          
             if(currentitem.type == type){
               Fernverkehr++;
@@ -91,11 +101,9 @@ window.onload = function(){
         }
         Klassifizierung.push(["Fernverkehr",Fernverkehr]);
 
+        //Adding the Nahverkehr Counter to the List
         var Nahverkehr = 0;
         for (type of NVFilter){
-
-          
-          
           for (currentitem of result){          
             if(currentitem.type == type){
               Nahverkehr++;
@@ -105,6 +113,7 @@ window.onload = function(){
         }
         Klassifizierung.push(["Nahverkehr",Nahverkehr]);
 
+        //Adding the Bus-SEV Counter to the List
         var Bus = 0;
         for (type of BusFilter){
 
@@ -118,8 +127,7 @@ window.onload = function(){
         }
         Klassifizierung.push(["Bus",Bus]);
 
-        console.log(Klassifizierung);
-
+        //Create the table for the Train Types
         for (item of sortedtypelistwithcounter){
 
           var table = document.getElementById("traintypetable");
@@ -145,6 +153,7 @@ window.onload = function(){
 
         }
 
+        //Create the Table for the Places
         for (item of sortedcitylistwithcounter){
 
           var table = document.getElementById("citytable");
@@ -169,10 +178,13 @@ window.onload = function(){
           table.appendChild(row);
 
         }
+
+        //Load google charts script
         google.charts.load('current', {'packages':['corechart']});
         google.charts.setOnLoadCallback(drawChart);
         function drawChart() {
 
+          //Data for Zugarten Pie Chart
           var datatype = new google.visualization.DataTable();
           datatype.addColumn('string', 'word');
           datatype.addColumn('number', 'count');
@@ -180,14 +192,16 @@ window.onload = function(){
             datatype.addRow([item[1] , item[0]]);
           }
 
+          //Options for Zugarten Pie Chart
           var optionstype = {
             title:'Zugarten'
           };
           
+          //Creation of Zugarten Pie Chart
           var charttype = new google.visualization.PieChart(document.getElementById('TypeChart'));
           charttype.draw(datatype, optionstype);
 
-
+          //Data for City Pie Chart
           var datacity = new google.visualization.DataTable();
           datacity.addColumn('string', 'word');
           datacity.addColumn('number', 'count');
@@ -195,13 +209,16 @@ window.onload = function(){
             datacity.addRow([item[1] , item[0]]);
           }
 
+          //Options for City Pie Chart
           var optionscity = {
             title:'Städte'
           };
           
+          //creation of city pie chart
           var chartcity = new google.visualization.PieChart(document.getElementById('CityChart'));
           chartcity.draw(datacity, optionscity);
           
+          //data for klassifizierungs chart
           var datacomparison = new google.visualization.DataTable();
           datacomparison.addColumn('string', 'word');
           datacomparison.addColumn('number', 'Anzahl Fahrten');
@@ -209,10 +226,12 @@ window.onload = function(){
             datacomparison.addRow([item[0] , item[1]]);
           }
 
+          //options for klassifizierungschart
           var optionsccomparison = {
             title:'Zugklasse'
           };
           
+          //creation of klassifizierungschart
           var chart = new google.visualization.BarChart(document.getElementById('myChart'));
           chart.draw(datacomparison, optionsccomparison);
 
