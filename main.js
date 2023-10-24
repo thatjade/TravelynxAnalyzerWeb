dda4c0472591bb262481f584330fb49f56bb35ac
Lola <mail@1lola.me>
2023-10-24 19:24:24 +0200

    added STR and U Datatype to Nahverkehr Filter

--- main.js
+++ main.js
@@ -121,7 +121,7 @@ window.onload = function(){
 
         //Filters for Classification
         var FVFilter = ["ICE","IC","FLX","EC","ECE","THA"];
-        var NVFilter = ["RB","HLB","S","RE","VIA","RT","TL","FEX","ME","WFB","TLX"];
+        var NVFilter = ["RB","HLB","S","RE","VIA","RT","TL","FEX","ME","WFB","TLX","STR","U"];
         var BusFilter = ["Bus"];
 
         //Adding the Fernverkehr Counter to the List
